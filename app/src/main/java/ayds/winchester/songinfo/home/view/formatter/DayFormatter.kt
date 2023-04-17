c07949e6c82dbc7f7946993bb32075cd49aab173
EzePerez <ezequielalejandroperez@hotmail.com>
2023-04-17 15:07:44 -0300

    Se modifico la clase DayFormatter

--- app/src/main/java/ayds/winchester/songinfo/home/view/formatter/DayFormatter.kt
+++ app/src/main/java/ayds/winchester/songinfo/home/view/formatter/DayFormatter.kt
@@ -1,12 +1,11 @@
 package ayds.winchester.songinfo.home.view.formatter
 
-
 class DayFormatter: PrecisionFormatter {
 
-    override fun formatWithPrecision(date:String):String=
-        "${getDaySubstring(date)}/${getMonthSubstring(date)}/${getYearSubstring(date)}"
-
-    private fun getDaySubstring(date: String) = date.split("-")[2]
-    private fun getMonthSubstring(date: String) = date.split("-")[1]
-    private fun getYearSubstring(date: String) = date.split("-")[0]
+    override fun formatWithPrecision(date:String):String {
+        val day = date.split("-")[2]
+        val month = date.split("-")[1]
+        val year = date.split("-")[0]
+        return "$day/$month/$year"
+    }
 }
