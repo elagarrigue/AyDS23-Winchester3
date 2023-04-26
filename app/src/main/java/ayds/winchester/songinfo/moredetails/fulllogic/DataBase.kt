2e4d103b1f0c87f5f25c096ba103ed775a64322c
hernanpow <perezhernan59@gmail.com>
2023-04-26 08:31:05 -0300

    Correciones PR Se elimino companion object

--- app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/DataBase.kt
@@ -7,9 +7,7 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
 import java.io.IOException
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
+
 
 private const val ARTISTS_TABLE = "artists"
 private const val ID_COLUMN = "id"
@@ -30,7 +28,7 @@ private const val createArtistInfoTableQuery: String =
             "$INFO_COLUMN string" +
             "$SOURCE_COLUMN)"
 
-private const val DATABASE_NAME="artist.db" //"dictionary.db"
+private const val DATABASE_NAME="artist.db"
 private const val DATABASE_VERSION= 1
 
 private var dataBase: DataBase = TODO()
@@ -42,7 +40,7 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, nul
              db.execSQL(createArtistInfoTableQuery)
             Log.i("DataBaseArtist", "ArtistDataBase Created OK")
         }catch ( error : IOException){
-            Log.e("DataBaseArtist", "ArtisDataBase error : "+ error.message);
+            Log.e("DataBaseArtist", "ArtisDataBase error : "+ error.message)
         }
     }
 
@@ -50,70 +48,42 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, nul
 
     }
 
-    companion object {
-       fun testDB() {
-            var connection: Connection? = null
-            try {
-                connection = DriverManager.getConnection("jdbc:sqlite:./$DATABASE_NAME")
-                val statement = connection.createStatement()
-                statement.queryTimeout = 30
 
-                val rs = statement.executeQuery("select * from $ARTISTS_TABLE")
-                while (rs.next()) {
-                    Log.i("DataBaseArtist", "$ID_COLUMN = " + rs.getInt(ID_COLUMN))
-                    Log.i("DataBaseArtist","$ARTIST_COLUMN = " + rs.getString(ARTIST_COLUMN))
-                    Log.i("DataBaseArtist","$INFO_COLUMN = " + rs.getString(INFO_COLUMN))
-                    Log.i("DataBaseArtist","$SOURCE_COLUMN = " + rs.getString(SOURCE_COLUMN))
-                }
-            } catch (error: SQLException) {
-                Log.e("DataBaseArtist", "ArtisDataBase error: "+error.message)
-            } finally {
-                try {
-                    connection?.close()
-                } catch (error: SQLException) {
-                    Log.e("DataBaseArtist", "ArtisDataBase error: "+error.message)
-                }
-            }
-        }
+    fun saveArtist(artist: String?, info: String?) {
 
-        @JvmStatic
-        fun saveArtist(artist: String?, info: String?) {
+        val values = ContentValues()
+        values.put(ARTIST_COLUMN, artist)
+        values.put(INFO_COLUMN, info)
+        values.put(SOURCE_COLUMN, 1)
 
-            val values = ContentValues()
-            values.put(ARTIST_COLUMN, artist)
-            values.put(INFO_COLUMN, info)
-            values.put(SOURCE_COLUMN, 1)
+        dataBase.writableDatabase?.insert(ARTIST_COLUMN, null, values)
+    }
 
-            dataBase.writableDatabase?.insert(ARTIST_COLUMN, null, values)
-        }
 
-        @JvmStatic
-        fun getInfo(artist: String): String? {
+    fun getInfo(artist: String): String? {
 
-            val cursor = dataBase.readableDatabase.query(
-                ARTISTS_TABLE,
-                projection,
-                "$ARTIST_COLUMN = ?",
-                arrayOf(artist),
-                null,
-                null,
-                "artist DESC"
+        val cursor = dataBase.readableDatabase.query(
+            ARTISTS_TABLE,
+            projection,
+            "$ARTIST_COLUMN = ?",
+            arrayOf(artist),
+            null,
+            null,
+            "artist DESC"
+        )
+        return mapArtistInfo(cursor)
+    }
+
+    private fun mapArtistInfo(cursor: Cursor): String? {
+        val items: MutableList<String> = ArrayList()
+        while (cursor.moveToNext()) {
+            val info = cursor.getString(
+                cursor.getColumnIndexOrThrow("info")
             )
-            return mapArtistInfo(cursor)
+            items.add(info)
         }
-
-        private fun mapArtistInfo(cursor: Cursor): String? {
-            val items: MutableList<String> = ArrayList()
-            while (cursor.moveToNext()) {
-                val info = cursor.getString(
-                    cursor.getColumnIndexOrThrow("info")
-                )
-                items.add(info)
-            }
-            cursor.close()
-            return if (items.isEmpty()) null else items[0]
-        }
-
+        cursor.close()
+        return if (items.isEmpty()) null else items[0]
     }
 
 }
