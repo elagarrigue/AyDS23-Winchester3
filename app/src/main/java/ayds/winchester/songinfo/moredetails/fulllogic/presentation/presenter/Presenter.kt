5b67350d2fbb37cde3ffd55556a84402a5b14f4b
Giuliana Guidi <89464824+giulianaguidi@users.noreply.github.com>
2023-05-08 16:23:16 -0300

    Se implemento el Observer en OtherInfoWindowView y Presenter

--- app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/presentation/presenter/Presenter.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/presentation/presenter/Presenter.kt
@@ -1,18 +1,25 @@
 package ayds.winchester.songinfo.moredetails.fulllogic.presentation.presenter
+import ayds.observer.Observable
+import ayds.observer.Subject
 import ayds.winchester.songinfo.moredetails.fulllogic.OtherInfoWindowUIState
 import ayds.winchester.songinfo.moredetails.fulllogic.domain.Repository
 import ayds.winchester.songinfo.moredetails.fulllogic.domain.entities.WikipediaArtist
 
 
 interface Presenter {
+    val uiStateObservable: Observable<OtherInfoWindowUIState>
     fun showArtistInfo(artistName:String)
 }
 
-class PresenterImpl(private val artistRepository: Repository, val formatter: ArtistDescriptionFormatterHtml):Presenter {
+class PresenterImpl(private val artistRepository: Repository, private val formatter: ArtistDescriptionFormatterHtml):Presenter {
+
+    private val onUIStateSubject = Subject<OtherInfoWindowUIState>()
+    override val uiStateObservable:Observable<OtherInfoWindowUIState> = onUIStateSubject
 
     override fun showArtistInfo(artistName:String) {
         val artist = artistRepository.getArtistInfoFromRepository(artistName)
         val uiState = createUIState(artist)
+        onUIStateSubject.notify(uiState)
     }
 
     private fun createUIState(artist: WikipediaArtist?): OtherInfoWindowUIState {
