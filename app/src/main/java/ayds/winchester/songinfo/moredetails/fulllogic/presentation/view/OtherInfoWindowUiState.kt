fce326aac7ec4639ed87396886aef9cdb3b33f3e
Giuliana Guidi <89464824+giulianaguidi@users.noreply.github.com>
2023-05-08 14:57:51 -0300

    Creacion packages view y presenter Se creo la clase ArtistDescriptionFormatterHtml en la que se formatea la informacion del artista.

--- app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/presentation/view/OtherInfoWindowUiState.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/presentation/view/OtherInfoWindowUiState.kt
@@ -1,4 +1,4 @@
-package ayds.winchester.songinfo.moredetails.fulllogic.presentation
+package ayds.winchester.songinfo.moredetails.fulllogic.presentation.view
 
 private const val WIKIPEDIA_LOGO_URL = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Wikipedia-logo-v2-es.png"
 
