2d6ac3fa72407b33a5bc167bb0da2eab35754be6
EzePerez <ezequielalejandroperez@hotmail.com>
2023-05-27 16:51:46 -0300

    Implementacion de la clase broker

--- app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/data/externalServices/Broker.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/data/externalServices/Broker.kt
@@ -1,5 +1,6 @@
 package ayds.winchester.songinfo.moredetails.fulllogic.data.externalServices
 
+import ayds.winchester.songinfo.moredetails.fulllogic.data.externalServices.proxys.ProxyService
 import ayds.winchester.songinfo.moredetails.fulllogic.domain.entities.Card
 
 interface Broker{
@@ -7,4 +8,21 @@ interface Broker{
     fun getArtistFromExternalServices(artistName: String):Collection<Card>
 }
 
-//TODO brokerImpl
+internal class BrokerImpl:Broker{
+
+    private lateinit var proxyServices : MutableCollection<ProxyService>
+
+    override fun addProxy(proxyService: ProxyService){
+        proxyServices.add(proxyService)
+    }
+
+    override fun getArtistFromExternalServices(artistName: String): Collection<Card>{
+        lateinit var collectionCards : MutableCollection<Card>
+        proxyServices.forEach {
+            it.getArtistInfo(artistName)?.let {
+                    card -> collectionCards.add(card)
+            }
+        }
+        return collectionCards
+    }
+}
